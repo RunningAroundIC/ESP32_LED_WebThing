961c4dba0b8daf5301aeeb66ff955f6169d74442
Karsten Darré <karsten@raic.dk>
2020-12-06 16:35:04 +0100

    Refactured and added functionality

--- ESP32_LED_WebThing.ino
+++ ESP32_LED_WebThing.ino
@@ -27,8 +27,77 @@ const char *deviceTypes[] = {"Light", "OnOffSwitch", "ColorControl", nullptr};
 ThingDevice device("StormTrooperLamp", "Controllable rgb lamp, there are opetions to dim and change color on this lamp.", deviceTypes);
 // Property
 ThingProperty deviceOn("on", "Whether the led is turned on or off", BOOLEAN, "OnOffProperty");
-ThingProperty deviceLevel("level", "The brightness of the light from 0-100", NUMBER, "BrightnessProperty");
+ThingProperty deviceLevel("level", "The brightness of the light from 0-255", NUMBER, "BrightnessProperty");
 ThingProperty deviceColor("color", "The color of light in RGB", STRING, "ColorProperty");
+ThingProperty remberColor("no", "Whether to rember the given color on next startup", BOOLEAN, "BooleanProperty");
+
+// ### Functions to be used ###
+
+// A startup "effect" each led will glow green for N miliseconds, and then turn off.
+void ledStartup(uint8_t wait) {
+  Serial.println("Running startup LED sequece...");
+  for(int i=0; i < strip.numPixels(); i++){
+    strip.setPixelColor(i, 0, 255, 0);
+    strip.show();
+    delay(wait);
+    strip.setPixelColor(i, 0, 0, 0);
+    strip.show();
+  }
+}
+
+// Simple led blink function to be used in a loop
+void ledBlink(uint8_t wait, uint16_t ledNumber, uint32_t color) {
+  strip.setPixelColor(ledNumber, color);
+  strip.show();
+  delay(wait);
+  strip.setPixelColor(ledNumber, 0, 0, 0);
+  strip.show();
+}
+
+void update(String *color) {
+  int red, green, blue;
+  if (color && (color->length() == 7) && color->charAt(0) == '#') {
+    const char *hex = 1 + (color->c_str()); // skip leading '#'
+    sscanf(0 + hex, "%2x", &red);
+    sscanf(2 + hex, "%2x", &green);
+    sscanf(4 + hex, "%2x", &blue);
+  }
+
+  if (red && green && blue) {
+    uint32_t rgbcolor = strip.gamma32(strip.Color(red, green, blue));
+    strip.fill(rgbcolor);
+    strip.show();
+  }  
+}
+
+// Test Wheel and rainbow can be deleted
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
 
 // Setup
 void setup() {
@@ -49,17 +118,18 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   const uint32_t ledBlinkColor = strip.Color(255, 0, 0);
-  while (WiFi.status() != WL_CONNECTED){
+  unsigned long start = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - start <= 60000){
     Serial.print(".");
-    ledBlink(70, 6, ledBlinkColor);
-    delay(350);
+    ledBlink(70, (strip.numPixels() / 2), ledBlinkColor);
+    delay(330);
   }
   Serial.println("");
 
-  if(WiFi.status() == WL_CONNECTED){
+  if(WiFi.status() == WL_CONNECTED) {
     connected = true;
     const uint32_t ledFillColor = strip.Color(22, 181, 147);
-    for(int i=0; i < 4; i++){
+    for(int i=0; i < 4; i++) {
       strip.fill(ledFillColor);
       strip.show();
       delay(90);
@@ -74,27 +144,46 @@ void setup() {
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
 
+    // init webthings adaptor
     adapter = new WebThingAdapter("rgbLamp", WiFi.localIP());
 
     // Setting values on the properties
     deviceLevel.minimum = 0;
-    deviceLevel.maximum = 100;
-    deviceLevel.unit = "percent";
+    deviceLevel.maximum = 255;
+    deviceLevel.unit = "brightness";
+
     // Adding properties
     device.addProperty(&deviceOn);
     device.addProperty(&deviceLevel);
     device.addProperty(&deviceColor);
+    device.addProperty(&remberColor);
+
     // Adding device
     adapter->addDevice(&device);
+
     // Start the adaptor
     adapter->begin();
+
     Serial.println("HTTP server started");
     Serial.print("http://");
     Serial.print(WiFi.localIP());
     Serial.print("/things/");
     Serial.println(device.id);
   }else{
-    Serial.println("Failed to connect to Wifi!");
+    Serial.println("Failed connecting to Wifi!");
+    Serial.println("Please check your network, and retry.");
+  }
+
+  if (!connected)
+  {
+    const uint32_t ledErrorColor = strip.Color(255, 0, 0);
+    while (true)
+    {
+      for(int i=0; i < strip.numPixels(); i++){
+        ledBlink(140, i, ledErrorColor);
+      }
+      delay(500);
+    }    
   }
 
   delay(500);
@@ -102,58 +191,20 @@ void setup() {
   strip.show();
 }
 
-// A startup "effect" each led will glow green for N miliseconds, and then turn off.
-void ledStartup(uint8_t wait){
-  Serial.println("Running startup LED sequece...");
-  for(int i=0; i < strip.numPixels(); i++){
-    strip.setPixelColor(i, 0, 255, 0);
-    strip.show();
-    delay(wait);
-    strip.setPixelColor(i, 0, 0, 0);
-    strip.show();
-  }
-}
-
-// Simple led blink function to be used in loop
-void ledBlink(uint8_t wait, uint16_t ledNumber, uint32_t color){
-  strip.setPixelColor(ledNumber, color);
-  strip.show();
-  delay(wait);
-  strip.setPixelColor(ledNumber, 0, 0, 0);
-  strip.show();
-}
-
 // Main code loop
 void loop(){
-  if(connected){
-    rainbow(20);
-  }  
-}
+  adapter->update();
 
-// Test
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
+  // On/Off
+  if (deviceOn.getValue().boolean) {
+    strip.setBrightness(deviceLevel.getValue().integer);
     strip.show();
-    delay(wait);
+  }else {
+    strip.setBrightness(0);
+    strip.show();
   }
-}
+  
+  // Set color
+  update(deviceColor.getValue().string);
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
